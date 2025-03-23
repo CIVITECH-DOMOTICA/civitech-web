84ad5cde78b26efeab7175d0f8403fbe1d4628c8
Tatiana Limache <tati54che@gmail.com>
2025-03-23 02:49:52 +0100

    Se actualizo rutas en imagenes, se actualizo el responsive xs y sm en formulario de contactanos

--- src/app/features/proyectos/proyectos.component.ts
+++ src/app/features/proyectos/proyectos.component.ts
@@ -12,17 +12,17 @@ export class ProyectosComponent {
   opiniones = [
 
     {
-      src:'../../../../assets/images/cliente1.jpg',
+      src:'/../../../../assets/images/cliente1.jpg',
       nombre: 'Domotizacion Casa Rural en Asturias',
       resenia: 'Nos especializamos en Domotica con enfoque en estilo, funcionalidad y soluciones tecnológicas. '
     },
     {
-      src:'../../../../assets/images/cliente3.jpg',
+      src:'/../../../../assets/images/cliente3.jpg',
       nombre: 'Monitorizacion de Consumo Energetico Chalet en Barcelona',
       resenia: 'Nos especializamos en Domotica con enfoque en estilo, funcionalidad y soluciones tecnológicas. '
     },
     {
-      src:'../../../../assets/images/cliente2.jpg',
+      src:'/../../../../assets/images/cliente2.jpg',
       nombre: 'Domotizacion Piso en Castellon de la Plana',
       resenia: 'Nos especializamos en Domotica con enfoque en estilo, funcionalidad y soluciones tecnológicas. '
     }
